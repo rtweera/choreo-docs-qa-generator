813de45608c7998c51d20337de2750a95dfcd306
Ravindu Weerasinghe <ravindutharuka2001@gmail.com>
2025-05-14 11:11:27 +0530

    Add logger implementation and integrate into main execution flow

---
+++ app/logger.py
@@ -0,0 +1,36 @@
+import logging
+import os
+
+class Logger():
+    def __init__(self, logger_name, log_filename, level=logging.INFO, log_dir='logs'):
+        self.logger = logging.getLogger(logger_name)
+        self.logger.setLevel(level=level)
+        
+        os.makedirs(log_dir, exist_ok=True)
+        filepath = os.path.join(log_dir, log_filename)
+        file_handler = logging.FileHandler(filename=filepath)
+        file_handler.setLevel(level=level)
+        file_formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+        file_handler.setFormatter(file_formatter) 
+
+        stream_handler = logging.StreamHandler()
+        stream_handler.setLevel(logging.INFO)
+        stream_formatter = logging.Formatter('%(levelname)s - %(message)s')
+        stream_handler.setFormatter(stream_formatter)
+
+        # Add handlers if not already added
+        if not self.logger.hasHandlers():
+            self.logger.addHandler(file_handler)
+            self.logger.addHandler(stream_handler)
+        else:
+            # Avoid duplicate logs if rerun in some environments
+            self.logger.handlers.clear()
+            self.logger.addHandler(file_handler)
+            self.logger.addHandler(stream_handler)
+
+    def __getattr__(self, name):
+        """
+        Allow us to call the method we pass into the object (like `.info()`, `error()`) on the original 
+        logger object. (saves us from needing to redefine the methods again)
+        """
+        return getattr(self.logger, name)
--- main.py
+++ main.py
@@ -1,9 +1,25 @@
+import argparse
+
 from app import directory_sum
 from config import config
 from app.llm import model_health_check
+from app.logger import Logger
 
-# directory_sum.sum(config.DOCS_DIR)
+logger = Logger(__name__, 'main_runs.log')
 
-# directory_sum.summarize(config.DOCS_DIR)
+def default_flow():
+    directory_sum.sum(config.DOCS_DIR)
+    directory_sum.summarize(config.DOCS_DIR)
 
-print(model_health_check())
+def main():
+    parser = argparse.ArgumentParser(description="Choreo Docs QA Generator")
+    parser.add_argument("--health", action="store_true", help="Run the health check")
+    args = parser.parse_args()
+
+    if args.health:
+        logger.info(model_health_check())
+    if not (args.health):
+        default_flow()
+
+if __name__ == '__main__':
+    main()
