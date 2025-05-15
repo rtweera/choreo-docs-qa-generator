603e478e40869554e12a6d94c976b05ae7ce98f2
Ravindu Weerasinghe <ravindutharuka2001@gmail.com>
2025-05-15 15:10:18 +0530

    Refactor main.py to integrate qa_generator and choreo_chat modules; update argument parsing for question generation, answer retrieval, and chat functionality.

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import argparse
 
-from app import directory_sum, llm
+from app import directory_sum, llm, qa_generator
+from app.choreo import choreo_chat
 from config import config
 from app.logger import Logger
 
 health_logger = Logger('health', "main_runs.log")
 llm_logger = Logger('llm', 'llm_runs.log')
-
+choreo_logger = Logger('choreo', 'choreo_chat.log')
 
 def full_flow():
     directory_sum.sum(config.DOCS_DIR)
@@ -21,8 +22,9 @@ def main():
         "--concat", action="store_true", help="Run the final concatenation only"
     )
     parser.add_argument("--question", action="store_true", help="Run the question generation")
+    parser.add_argument("--chat", action="store_true", help="Run the choreo chat")
     parser.add_argument("--all", action="store_true", help="Run the entire flow")
-
+    parser.add_argument("--answer", action="store_true", help="Run the answer generation from questions")
     args = parser.parse_args()
 
     if args.health:
@@ -30,8 +32,14 @@ def main():
     if args.concat:
         directory_sum.concat_summaries(config.DOCS_DIR)
     if args.question:
-        health_logger.info('LLM run: Question generation')
-        llm_logger.info(llm.generate_questions())
+        health_logger.info('Question run: Question generation')
+        # llm_logger.info(llm.generate_questions())
+        qa_generator.run_generate_questions()
+    if args.answer:
+        health_logger.info('Answer run: Answer generation')
+        qa_generator.find_answers()
+    if args.chat:
+        choreo_logger.info(choreo_chat.ask_question('what is choreo?'))
     if args.all:
         full_flow()
 
